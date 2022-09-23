8062f8012933edbefdc8e42df617d7c5651c9325
Pelayo <phuermij@myuax.com>
2022-09-23 16:37:31 +0200

    Ejercicio 3 recursivo

--- Ejercicio3.py
+++ Ejercicio3.py
@@ -1,3 +1,26 @@
 def rango(a, b, c):
     for i in range(a, b, c):
         print(i)
+
+#Forma recursiva
+
+def rango_recursivo(a, b, c):
+    n = b - a
+    i = 0
+
+    if n == 0:
+        return print(a)
+    
+    elif i == 0:
+        print(b)
+        i = i + 1
+        return rango_recursivo(a, b - c, c)
+
+    else:
+        n = b - c
+        i = i + 1
+        print(n)
+        return rango_recursivo(a, b - c, c)
+
+rango_recursivo(0, 20, 5)
+
