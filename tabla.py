2ed18af414f284110926c2d55ece0c01e9d695c1
Pelayo <phuermij@myuax.com>
2022-09-23 16:21:12 +0200

    Ejercicio 4

--- tabla.py
+++ tabla.py
@@ -1,11 +1,19 @@
+def comprobacion(a):
+    if 1 <= a <= 9:
+        pass
+    else:
+        raise ValueError("Los números tienen que estar comprendidos entre 1 y 9")
+
 def tabla(a, b):
     if a == "" or b == "":
         print("Hay que meter dos números entre el 1 y el 9") 
-    if 1 <= a >= 9:
-        pass
-    else:
-        raise ValueError("Los números tienen que estar comprendidos entre 1 y 9")
-    if 1 <= b >= 9:
-        pass
-    else:
-        raise ValueError("Los números tienen que estar comprendidos entre 1 y 9")
+    
+    comprobacion(a)
+    comprobacion(b)
+
+    for i in range(a):
+        for j in range(b):
+            print("*",end="")
+
+tabla(2,2)
+
